af6613435f60cab5f7545fdb786fb64ef35cb86e
lokeshwarlakhi <lokeshwarlakhi@gmail.com>
2021-03-10 12:28:08 +0530

    this is updated

--- 1toNamstrong.c
+++ 1toNamstrong.c
@@ -1,31 +1,30 @@
-#include <stdio.h>
-#include <conio.h>
-
-void main()
-{
-
-    int n;
-    int temp ;
-    int fact=1;
-    scanf("%d", &n);
-
-    for (int i = 1; i < n; i++)
-    {
-        while (i != 0)
-        {
-            int temp = i % 10;
-            for (int j = (temp - 1); j >= 1; j--)
-            {
-                temp *= j;
-            }
-            fact += temp;
-            i = i / 10;
-        }
-        if (fact == i)
-        {
-            printf("%d", i);
-        }
-    }
-
-    getch();
-}
+#include<stdio.h>  
+#include<conio.h>
+  
+void main()  
+{  
+    int num, count = 1, rem, sum,n;
+    scanf("%d",&n);  
+  
+    while(count <= n)  
+    {  
+        num = count;  
+        sum = 0;  
+  
+        while(num)  
+        {  
+            rem = num % 10;  
+            sum = sum + (rem * rem * rem);  
+            num = num / 10;  
+        }  
+  
+        if(count == sum)  
+        {  
+            printf("%d is a Armstrong number\n", count);  
+        }  
+  
+        count++;  
+    }  
+  
+     getch();  
+} 
